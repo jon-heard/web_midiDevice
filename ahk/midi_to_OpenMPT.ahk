76040a24dd6b24e9bad6dabd06a5464191ea4b25
Jonathan Heard <jon.heard.tutor@gmail.com>
2023-09-22 13:22:40 -0400

    Initial commit

---
+++ ahk/midi_to_OpenMPT.ahk
@@ -0,0 +1,72 @@
+IfWinNotExist, OpenMPT
+{
+	MsgBox OpenMPT window not found.\nQuitting AHK script.
+	ExitApp
+}
+
+#include Midi.ahk
+midi := new Midi()
+midi.OpenMidiIn(0)
+Return
+
+MidiControlChange119:
+	value := midi.MidiIn().value
+	Switch midi.MidiIn().value
+	{
+		case 0:
+			ControlSend, AfxFrameOrView140su2, {Up down}, OpenMPT
+			return
+		case 1:
+			ControlSend, AfxFrameOrView140su2, {Up up}, OpenMPT
+			return
+		case 2:
+			ControlSend, AfxFrameOrView140su2, {Down down}, OpenMPT
+			return
+		case 3:
+			ControlSend, AfxFrameOrView140su2, {Down up}, OpenMPT
+			return
+		case 4:
+			ControlSend, AfxFrameOrView140su2, {Left down}, OpenMPT
+			return
+		case 5:
+			ControlSend, AfxFrameOrView140su2, {Left up}, OpenMPT
+			return
+		case 6:
+			ControlSend, AfxFrameOrView140su2, {Right down}, OpenMPT
+			return
+		case 7:
+			ControlSend, AfxFrameOrView140su2, {Right up}, OpenMPT
+			return
+		case 8:
+			ControlSend, AfxFrameOrView140su2, {= down}, OpenMPT
+			return
+		case 9:
+			ControlSend, AfxFrameOrView140su2, {= up}, OpenMPT
+			return
+/*
+// Unused ATM
+		case 10:
+			ControlSend, AfxFrameOrView140su2, {Shift down}, OpenMPT
+			return
+		case 11:
+			ControlSend, AfxFrameOrView140su2, {Shift up}, OpenMPT
+			return
+*/
+		case 12:
+			ControlSend, AfxFrameOrView140su2, {F12}{Del}, OpenMPT
+			return
+		case 13:
+			return
+		case 14:
+			ControlSend, AfxFrameOrView140su2, {F6 down}, OpenMPT
+			return
+		case 15:
+			ControlSend, AfxFrameOrView140su2, {F6 up}, OpenMPT
+			return
+		case 16:
+			ControlSend, AfxFrameOrView140su2, ^{Home}, OpenMPT
+			return
+		case 17:
+			return
+	}
+	return
---
+++ libs/VibratoMaker.js
@@ -0,0 +1,63 @@
+
+class VibratoMaker {
+	SPEED_ADJUST = .01;
+	DEPTH_ADJUST = .15;
+
+	enabled = true;
+	pitchRange = 1;
+	basePitch = 0.5;
+	speed = .5;
+	depth = .5;
+	priorTime = 0;
+	timing = 0;
+
+	constructor(midiChannels) {
+		this.midiChannels = midiChannels;
+		this.boundFnc_frame = this.frame.bind(this);
+		requestAnimationFrame(this.boundFnc_frame);
+	}
+
+	frame(time) {
+		if (!this.enabled) return;
+		let finalPitch = this.basePitch;
+		if (this.speed) {
+			this.timing += (time - this.priorTime) / this.speed * this.SPEED_ADJUST;
+			this.priorTime = time;
+			const vibratoAdjust = Math.sin(this.timing) * this.depth * this.DEPTH_ADJUST;
+			finalPitch = this.basePitch + vibratoAdjust / this.pitchRange;
+		}
+		finalPitch = Math.min(Math.max(finalPitch, 0), 1);
+		finalPitch = finalPitch * 2 - 1;
+		for (let channelId = 0; channelId < this.midiChannels.length; channelId++) {
+			midiChannels[channelId].sendPitchBend(finalPitch);
+		}
+		requestAnimationFrame(this.boundFnc_frame);
+	}
+
+	setEnabled(value) {
+		if (this.enabled === value) return;
+		this.enabled = value;
+		if (value) {
+			requestAnimationFrame(this.boundFnc_frame);
+		} else {
+			for (let channelId = 0; channelId < this.midiChannels.length; channelId++) {
+				midiChannels[channelId].sendPitchBend(this.basePitch * 2 - 1);
+			}
+		}
+	}
+
+	setPitchRange(value) { this.pitchRange = value; }
+
+	setBasePitch(value) {
+		this.basePitch = value;
+		if (!this.enabled) {
+			for (let channelId = 0; channelId < this.midiChannels.length; channelId++) {
+				midiChannels[channelId].sendPitchBend(this.basePitch * 2 - 1);
+			}
+		}
+	}
+
+	setSpeed(value) { this.speed = value; }
+
+	setDepth(value) { this.depth = value; }
+}
